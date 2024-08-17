462c50153078ac1f370cc44d08e5d327de2e6323
ahsanali159 <ahsanwarraich34@gmail.com>
2024-08-17 16:34:28 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import Textform from "./components/Textform";
+import Textform from "./components/Textform";
 function App() {
   return (
    <>
    <Navbar title="TextUtils" home="Home" about="About Us"/>
-    {/* <Textform title="Enter Text To Analyze"/> */}
+    <Textform title="Enter Text To Analyze"/>
     <About/>
    </>
   );
